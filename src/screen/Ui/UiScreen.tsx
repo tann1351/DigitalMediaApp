82ef64ecdf699502ba228a0538eb675b38ff46e8
tann1351 <tan.tanawat63@gmail.com>
2024-04-02 11:03:58 +0700

    up

--- src/screen/Ui/UiScreen.tsx
+++ src/screen/Ui/UiScreen.tsx
@@ -317,7 +317,8 @@ const styles = StyleSheet.create({
   },
   optionsContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-around',
+    justifyContent: 'flex-start',
+     
     width: '100%',
     marginTop: 8,
   },
@@ -325,6 +326,7 @@ const styles = StyleSheet.create({
     width: 65,
     height: 70,
     borderRadius: 10,
+    marginRight:10
   },
   divider: {
     height: 2,
